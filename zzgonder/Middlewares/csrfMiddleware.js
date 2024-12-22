6b43c8453e7d555c9597a40db425dfdbe0afabbd
Umut Şar <contact@umutsar.net>
2024-12-22 17:19:18 +0300

    Delete zzgonder directory

--- zzgonder/Middlewares/csrfMiddleware.js
+++
@@ -1,4 +0,0 @@
-const csrf = require("csurf")
-let csrfProtection = csrf({ cookie: true });
-
-module.exports = { csrfProtection };
--- zzgonder/routes/loginGetRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/login', (req, res) => {
-    res.render('login', { notActive: 0 });
-});
-
-
-module.exports = router;
